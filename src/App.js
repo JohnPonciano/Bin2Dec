28b17a8f37294a7d623250dff285835d859370c2
John <jhow.ponciano@hotmail.com>
2020-02-26 01:40:06 -0300

    Rodando

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Bin2dec from './components/bin2dec/Bin2dec.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Bin2dec binary="Binary" decimal="Decimal"/>
     </div>
   );
 }
--- src/components/bin2dec/Bin2dec.js
+++ src/components/bin2dec/Bin2dec.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 
-// import { Container } from './styles';
+import './styles.css';
 
 export default class Bin2dec extends Component {
 
@@ -11,18 +11,23 @@ state = {
 
     decimal_value: 0,
 
+    setErrorMessage:[],
 }
 
 converting = this.converting.bind(this)
 
 converting(){
 
+  
+
   //console.log("this.state")
- const reversedBinaryText = this.binary_value
+ const reversedBinaryText = this.state.binary_value
       .split('')
       .map(Number) // Convert to a number from string
       .reverse()
   
+  
+
   const result = reversedBinaryText.reduce(
       (accumulator, currentValue, idx) =>
         accumulator + currentValue * Math.pow(2, idx)
@@ -33,18 +38,20 @@ converting(){
 
   render() {
     return (
-        <div className='conversor-bin2dec'>
+        <body>
+          <div className='conversor-bin2dec'>
                 <h1> Bin2Dec</h1>
 
                 <h2> {this.props.binary} to {this.props.decimal} </h2>
 
-                <input type="text"  onChange={(event)=>{this.setState({binary_value:event.target.value})}}/>
+                <input type="text" placeholder="Enter 0 or 1" onChange={(event)=>{this.setState({binary_value:event.target.value})}}/>
                 
-                <input type="button" value="Convert" onClick={this.converting}/> 
+                <input type="button" className='Button'value="Convert" onClick={this.converting}/> 
                 
-  <h2>{this.state.decimal_value}</h2>
+                  <h2>{this.state.decimal_value}</h2>
 
         </div>
+        </body>
     )
   }
 }
